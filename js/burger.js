d207019208534fca0ad608656eef80ef842da20c
llogostevo <stevens.lloyd@gmail.com>
2018-04-03 18:25:57 +0100

    fixed burger menu, added form validation using JS and HTML5

--- js/burger.js
+++ js/burger.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
                     $("#main-nav").toggle(300);
                 });
             });
+
+// to capture a resize of the window to over 900 
+$( window ).resize(function(){
+    if (document.documentElement.clientWidth > 900) 
+        {
+                    $("#main-nav").show();
+        }
+});
---
+++ js/confirmemail.js
@@ -0,0 +1,13 @@
+var elem = document.getElementById("email_addr_confirm");
+elem.addEventListener("blur", verifyEmail);
+
+function verifyEmail(input) {
+  input = input.srcElement;
+  if (input.value != document.getElementById('email_addr').value) {
+    // the provided value doesn’t match the primary email address
+    input.setCustomValidity('The two email addresses must match.');
+  } else {
+    // input is valid -- reset the error message
+    input.setCustomValidity('');
+  }
+}
