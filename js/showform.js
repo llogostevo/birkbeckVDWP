c557667a1db9e3b10777e02ffee71d78a6bf57bf
llogostevo <stevens.lloyd@gmail.com>
2018-04-03 23:37:29 +0100

    maintenance on about us form, created basis of curriculum page

--- js/showform.js
+++ js/showform.js
@@ -3,38 +3,37 @@
 //the form changes as the function is changed
 $("#form-menu").change(function () {
             var choice = $( "#form-menu option:selected" ).val();
-            if(choice=="apply-now")
+            if(choice=="apply-now-form")
             {
               //show apply now fields
-               $("#event-signup").hide();
-                $("#apply-now").show();
-                $("#Contact-Us").hide();
-                $("#employers").hide();
+               $("#event-signup-form").hide();
+                $("#apply-now-form").show();
+                $("#Contact-Us-form").hide();
+                $("#employers-form").hide();
             }
-            else if(choice=="event-signup")
+            else if(choice=="event-signup-form")
             {
               //show event sign up fields
-               $("#event-signup").show();
-                $("#apply-now").hide();
-                $("#Contact-Us").hide();
-                $("#employers").hide();
+               $("#event-signup-form").show();
+                $("#apply-now-form").hide();
+                $("#Contact-Us-form").hide();
+                $("#employers-form").hide();
             }
-            else if(choice=="Contact-Us")
+            else if(choice=="Contact-Us-form")
             {
 //              show contact us fields
 //                NB this is currently empty and code is here purely for maintenance purposes to provide functionality in future, however the change will still hide the other fields that aren't relevant
-               $("#event-signup").hide();
-                $("#apply-now").hide();
+               $("#event-signup-form").hide();
+                $("#apply-now-form").hide();
 //                $("#Contact-Us").show();
-                $("#employers").hide();
+                $("#employers-form").hide();
             }
-            else if(choice=="employers")
+            else if(choice=="employers-form")
             {
               //show employers fields
-                $("#event-signup").hide();
-                $("#apply-now").hide();
-                $("#Contact-Us").hide();
-                $("#employers").show();
+                $("#event-signup-form").hide();
+                $("#apply-now-form").hide();
+                $("#Contact-Us-form").hide();
+                $("#employers-form").show();
             }
         });
-
