bd868b425a7659284ee70c57c874bbdc1feae1fe
llogostevo <stevens.lloyd@gmail.com>
2018-04-03 13:28:47 +0100

    alterations and corrections on burger menu, updated SSIs

--- js/burger.js
+++ js/burger.js
@@ -1,21 +1,6 @@
-// only run the following code if the viewport is less than 900
-window.onresize = function(){
-    
-    if (document.documentElement.clientWidth < 900) 
-        {
-
-            $(document).ready(function(){
-                //set nav to hide
-                $("#burger").hide();
+$(document).ready(function(){
                 //when clicked set nav to show
                 $("#burger-button").click(function(){
-                    $("#burger").toggle(300);
+                    $("#main-nav").toggle(300);
                 });
             });
-
-        }
-    else {
-        // used to reshow menu if the window has been resized above 900
-        $("#burger").show();
-    }
-}
---
+++ js/staff-toggle.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#stevens-text").hide();
+    $("#stevens-image").click(function(){
+        $("#stevens-text").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#tong-text").hide();
+    $("#tong-image").click(function(){
+        $("#tong-text").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#lau-text").hide();
+    $("#lau-image").click(function(){
+        $("#lau-text").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#vega-text").hide();
+    $("#vega-image").click(function(){
+        $("#vega-text").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#lassami-text").hide();
+    $("#lassami-image").click(function(){
+        $("#lassami-text").slideToggle("slow");
+    });
+});
