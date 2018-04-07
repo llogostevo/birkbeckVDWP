ff0b08eeae2bd3f77a7654678fbdf4c6bc4f60c3
llogostevo <stevens.lloyd@gmail.com>
2018-04-07 08:23:51 +0100

    completed layout for news, curriculum, about pages. updated form layout on about.

--- js/display-event.js
+++ js/display-event.js
@@ -1,6 +1,6 @@
 function displayEvent() {
     var eventChoice = document.getElementById("event-choice")
     var displayResult = eventChoice.options[eventChoice.selectedIndex].text;
-    document.getElementById("event-details").innerHTML = displayResult;
+    document.getElementById("form-event-details").innerHTML = displayResult;
 
 }
