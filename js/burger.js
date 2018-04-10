54101990b61e2c8d72379084e8efed66aa2c75d9
llogostevo <stevens.lloyd@gmail.com>
2018-04-10 08:47:56 +0100

    compressed images to progressive - some changes to code polish up

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-                //when clicked set nav to show
+                //when clicked set nav of main-nav id to show
                 $("#burger-button").click(function(){
+                    //toggle the main nav on or off, 300 is the speed to which the animation comes in
                     $("#main-nav").toggle(300);
                 });
             });
 
 // to capture a resize of the window to over 900 
 $( window ).resize(function(){
-    if (document.documentElement.clientWidth > 900) 
+    //check that the window width is greater than 900
+    if ( ($( window ).width()) > 900) 
         {
+            // if it is then show the main nav, turns it back on if it was in a hidden state from the above toggle function
                     $("#main-nav").show();
         }
 });
--- js/confirmemail.js
+++ js/confirmemail.js
@@ -3,7 +3,7 @@ elem.addEventListener("blur", verifyEmail);
 
 function verifyEmail(input) {
   input = input.srcElement;
-  if (input.value != document.getElementById('email_addr').value) {
+  if (input.value != document.getElementById('email').value) {
     // the provided value doesn’t match the primary email address
     input.setCustomValidity('The two email addresses must match.');
   } else {
